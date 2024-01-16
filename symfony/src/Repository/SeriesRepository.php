1a82581303e65c48bd29b42d57bd7556996a8720
Louis Michault <louis.michault@etu.u-bordeaux.fr>
2024-01-16 17:10:03 +0100

    Ajout de la progress bar et du nombre d'épisodes vus sur la liste des séries suivies

--- symfony/src/Repository/SeriesRepository.php
+++ symfony/src/Repository/SeriesRepository.php
@@ -30,13 +30,27 @@ class SeriesRepository extends ServiceEntityRepository
         )->leftJoin('App:Season', 'se', 'WITH', 's = se.series')->leftJoin('App:Episode', 'e', 'WITH', 'se = e.season')->leftJoin('s.genre', 'genre', 'WITH')->leftJoin('s.user', 'user', 'WITH')->groupBy('s.id')->orderBy('RAND('.$seed.')');
     }//end seriesInfo()
 
+    public function seriesEpisodesCount($user)
+    {
+        return $this->createQueryBuilder('s')->select(
+            's.id as id, s.title as title, s.poster, s.plot as plot, 
+            COUNT(DISTINCT se.number) as season_count, COUNT(Distinct(e.id)) as episode_count, s.youtubeTrailer'
+        )->leftJoin('App:Season', 'se', 'WITH', 's = se.series')->leftJoin('App:Episode', 'e', 'WITH', 'se = e.season')->leftJoin('s.genre', 'genre', 'WITH')->leftJoin('s.user', 'user', 'WITH')->groupBy('s.id')->where('user.id=:user')->setParameter('user', $user)->getQuery()->getResult();
+    }//end seriesAllInfo()
+
+    public function seriesEpisodeCountView($user)
+    {
+        return $this->createQueryBuilder('s')->select(
+            's.id as id, s.title as title, COUNT(Distinct(e.id)) as episode_count_view'
+        )->leftJoin('App:Season', 'se', 'WITH', 's = se.series')->leftJoin('App:Episode', 'e', 'WITH', 'se = e.season')->leftJoin('s.user', 'user', 'WITH')->groupBy('s.id')->where(':user MEMBER OF e.user')->setParameter('user', $user)->getQuery()->getResult();
+    }//end seriesAllInfo()
 
     public function seriesInfoById($id)
     {
         return $this->createQueryBuilder('s')->select('s')->leftJoin('s.seasons', 'seasons')->leftJoin('seasons.episodes', 'episode')->leftJoin('s.genre', 'genre', 'WITH')->leftJoin('s.user', 'user', 'WITH')->where('s.id = :id')->setParameter('id', $id)->orderBy('episode.number', 'ASC')->getQuery()->getOneOrNullResult();
     }//end seriesInfoById()
-
-
+    
+    
     public function seriesInfoByIdAndSeason($id, $num)
     {
         return $this->createQueryBuilder('s')->select('s')->leftJoin('s.seasons', 'seasons')->leftJoin('seasons.episodes', 'episode')->leftJoin('s.genre', 'genre', 'WITH')->leftJoin('s.user', 'user', 'WITH')->where('s.id = :id')->andWhere('seasons.number = :num')->setParameter('id', $id)->setParameter('num', $num)->getQuery()->getOneOrNullResult();
