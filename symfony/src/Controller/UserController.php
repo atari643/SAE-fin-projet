950d78c285942ab0da4cb74c02f23f132d36dbee
Hugo Martins <hugo.martins.1@etu.u-bordeaux.fr>
2024-01-17 10:29:42 +0100

    modifications des informations d'un utilisateur normalement fonctionnelle (reste probablement pb sécurité + affichage à mettre au propre

--- symfony/src/Controller/UserController.php
+++ symfony/src/Controller/UserController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use App\Entity\Rating;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SeriesRepository;
 
@@ -276,5 +278,38 @@ class UserController extends AbstractController
             'user' => $username,
             ]
         );
+    }
+
+    #[Route('/user/edit/', name: 'user_editor_edit', methods: ['GET', 'POST'])]
+    public function editProfile(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        if (null == $user) {
+            $login = $this->generateUrl('app_login');
+
+            return $this->redirect($login);
+        }
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid() && $user instanceof User) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_profile');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+            'errorN' => $form['name']->getErrors(true),
+            'errorP' => $form['plainPassword']->getErrors(true),
+        ]);
     }
 }
